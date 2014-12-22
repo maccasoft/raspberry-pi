e7079f417519a950800a39f7edb644eb3bda0b9e
Marco Maccaferri <marco.maccaferri@maccasoft.com>
2014-12-22 10:32:25 +0100

    Enabled support for long file names

--- kernel/fatfs/ffconf.h
+++ kernel/fatfs/ffconf.h
@@ -97,7 +97,7 @@
 /   1    - ASCII (No extended character. Valid for only non-LFN configuration.) */
 
 
-#define _USE_LFN    0
+#define _USE_LFN    3
 #define _MAX_LFN    255
 /* The _USE_LFN option switches the LFN feature.
 /
